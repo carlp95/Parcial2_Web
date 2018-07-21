22764c3c05ff319ebceef5263c72ac2baf0f4b68
ndanield <ndanield@hotmail.com>
2018-07-21 19:26:12 -0400

    verificacion de login lista

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -66,11 +66,9 @@ public class Main {
             Map<String, Object> model = new HashMap<>();
             if (!authenticate(request.queryParams("username"), request.queryParams("password"))) {
                 model.put("authenticationFailed", true);
-                System.out.println("auth failed!!");
                 return ViewUtil.render(request, model, Path.LOGIN);
             }
             model.put("authenticationSucceeded", true);
-            System.out.println("auth succeded!!");
             request.session().attribute("currentUser", request.queryParams("username"));
 
             // Si es necesario chekiar si esta logueado antes de una accion esto es util.
